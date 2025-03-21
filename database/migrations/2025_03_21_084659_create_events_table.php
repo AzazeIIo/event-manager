dd155dcf43fe60bbfc95d37307a56729a59305cc
AzazeIIo <61389155+AzazeIIo@users.noreply.github.com>
2025-03-21 10:33:57 +0100

    Created migrations

---
+++ database/migrations/2025_03_21_084659_create_events_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('image')->nullable();
+            $table->dateTime('date_start');
+            $table->dateTime('date_end')->nullable();
+            $table->string('description')->nullable();
+            $table->string('type')->nullable();
+            $table->string('city');
+            $table->string('location');
+            $table->foreignId('owner_id');
+            $table->boolean('is_public');
+            $table->timestamps();
+            $table->foreign('owner_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unique(['name', 'date_start']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
