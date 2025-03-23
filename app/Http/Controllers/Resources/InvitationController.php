e27ca4153346b796e69d30827f92c4b4d0336e59
AzazeIIo <61389155+AzazeIIo@users.noreply.github.com>
2025-03-23 10:55:08 +0100

    Created controllers and requests + moved a file

---
+++ app/Http/Controllers/Resources/InvitationController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Invitation;
+use Illuminate\Http\Request;
+
+class InvitationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreInvitationRequest $request)
+    {
+        $fields = $request->validated();
+        dd($fields);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Invitation $invitation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Invitation $invitation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Invitation $invitation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Invitation $invitation)
+    {
+        //
+    }
+}
---
+++ app/Http/Requests/StoreAttendeeRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreAttendeeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return (Auth::check() && Auth::user()->id == $this->request->get('user_id'));
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'user_id' => [
+                'required',
+                'exists:users,id',
+                Rule::unique('attendees', 'user_id')->where('event_id', $this->request->get('event_id'))
+            ],
+            'event_id' => [
+                'required',
+                'exists:events,id',
+            ]
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'user_id.unique' => 'You have already joined this event.'
+        ];
+    }
+}
