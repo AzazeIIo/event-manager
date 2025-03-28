c609562d26f4733fa2629ae26746c328a80be1e8
AzazeIIo <61389155+AzazeIIo@users.noreply.github.com>
2025-03-28 15:02:17 +0100

    Users can view all events they've joined

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\AttendeeController;
 use App\Models\Event;
 use App\Models\Type;
 use App\Models\Invitation;
+use App\Models\Attendee;
 
 Auth::routes();
 
@@ -14,7 +15,7 @@ Route::resource('events', EventController::class);
 Route::resource('events.attendees', AttendeeController::class);
 
 Route::redirect('/', 'events');
-Route::redirect('/joinedevents', 'myevents')->name('joinedevents');
+
 Route::get('/privateevents', function () {
     if(Auth::check()) {
         return View::make(request()->ajax() ? 'event-page' : 'events')->with([
@@ -26,6 +27,7 @@ Route::get('/privateevents', function () {
         return redirect('/login');
     }
 })->name('privateevents');
+
 Route::get('/myevents', function () {
     if(Auth::check()) {
         return View::make(request()->ajax() ? 'event-page' : 'events')->with([
@@ -37,4 +39,15 @@ Route::get('/myevents', function () {
         return redirect('/login');
     }
 })->name('myevents');
-//Route::get('/home', [EventController::class, 'index'])->name('home');
+
+Route::get('/joinedevents', function () {
+    if(Auth::check()) {
+        return View::make(request()->ajax() ? 'event-page' : 'events')->with([
+            'events' => Event::whereIn('id', (Attendee::where('user_id', '=', Auth::user()->id)->select('event_id')->get()))->paginate(10),
+            'types' => Type::all(),
+            'includeform' => false
+        ]);
+    } else {
+        return redirect('/login');
+    }
+})->name('joinedevents');
